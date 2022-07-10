3168d94a70218f5bca9091c722daefc315d35c8b
mohammad mahdi <mhgorgab@gmail.com>
2022-07-10 10:04:04 +0430

    bus fix , chenge DB_model to db_model

--- application/controllers/Project_C.php
+++ application/controllers/Project_C.php
@@ -4,14 +4,14 @@ class Project_C extends CI_Controller
 {
     public function index($p_name)
     {
-        $this->load->model('DB_model');
+        $this->load->model('db_model');
         $this->load->helper('url');
         $this->load->library(array('show_menu','read_image_folder'));
-        $project_data = $this->DB_model->get_projects_data($p_name);
+        $project_data = $this->db_model->get_projects_data($p_name);
         if (count($project_data) != 0) {
             $data = array(
                 'project_data' => $project_data,
-                'menus' => $this->DB_model->get_menus()
+                'menus' => $this->db_model->get_menus()
             );
             $this->load->view('project-page',$data);
         }else {
