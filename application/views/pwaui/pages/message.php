fb17f059b98167ecc00e4975f6e005950e07a29b
mohammad mahdi <mhgorgab@gmail.com>
2022-12-03 10:53:40 +0330

    add message baner for shop and message

---
+++ application/views/pwaui/pages/message.php
@@ -0,0 +1,7 @@
+<section class="py-2 container">
+	<div class="row align-items-center">
+		<div class="text-center col-lg-6 col-md-8 mx-auto ThemeStyle p-4 py-5 text-light animated bounceInUp">
+			<h3>این قابلیت در آینده فعال خواهد شد ...</h3>
+		</div>
+	</div>
+</section>
